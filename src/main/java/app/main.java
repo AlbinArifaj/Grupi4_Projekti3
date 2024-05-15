9643920e8314257ef0fd2c56960956c3d76ba502
AlbinArifaj <albiniarifaj@gmail.com>
2024-05-15 20:32:54 +0200

    Add InitialFiles

---
+++ src/main/java/app/main.java
@@ -0,0 +1,15 @@
+package app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Navigator.navigate(stage,Navigator.connect);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+//    opens com.example.demo to javafx.fxml;
+//    exports com.example.demo;
+
+    exports app;
+    opens controller to javafx.fxml;
+}
