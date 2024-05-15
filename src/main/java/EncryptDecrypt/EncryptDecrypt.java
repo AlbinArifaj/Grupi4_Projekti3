c07f7e6ea94b7c0fe53b2dff81dfda19c3242149
Alfredi <alfred.palokaj@studenti.uni-pr.edu>
2024-05-15 20:51:42 +0200

    Add Encrypt

---
+++ src/main/java/EncryptDecrypt/EncryptDecrypt.java
@@ -0,0 +1,38 @@
+package EncryptDecrypt;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+public class EncryptDecrypt {
+
+    private static String KEY = "password";
+    private static final String IV = "12345678"; // 8 bytes IV
+
+
+
+    public static byte[] encrypt(String plainText) throws Exception {
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        KeySpec keySpec = new DESKeySpec(KEY.getBytes());
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey secretKey = keyFactory.generateSecret(keySpec);
+        IvParameterSpec iv = new IvParameterSpec(IV.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey,iv);
+
+        byte[] paddedPlainText = padString(plainText);
+        return cipher.doFinal(paddedPlainText);
+    }
+
+    public  static byte[] padString(String text) {
+        int padding = 8 - (text.length() % 8);
+        StringBuilder stringBuilder = new StringBuilder(text);
+        for (int i = 0; i < padding; i++) {
+            stringBuilder.append((char) padding);
+        }
+        return stringBuilder.toString().getBytes();
+    }
+}
